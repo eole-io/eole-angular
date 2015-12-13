1ddb7d21faa957179d925909ff48f38533ad0557
Julien Maulny <doubjulien@hotmail.fr>
2015-12-13 18:32:20 +0100

    tictactoe controller

--- ng-eole/games/tictactoe/tictactoe.js
+++ ng-eole/games/tictactoe/tictactoe.js
@@ -7,16 +7,55 @@ ngEole.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
-ngEole.controller('TicTacToeController', ['$scope', '$routeParams', 'eoleApi', function ($scope, $routeParams, eoleApi) {
+ngEole.controller('TicTacToeController', ['$scope', '$routeParams', 'eoleApi', 'eoleWs', function ($scope, $routeParams, eoleApi, eoleWs) {
     var partyId = $routeParams.partyId;
 
+    $scope.Math = Math;
     $scope.party = null;
     $scope.game = null;
+    $scope.tictactoe = {
+        grid: '-'.repeat(9),
+        last_move: null
+    };
 
-    eoleApi.getParty('tictactoe', partyId).then(function (party) {
-        console.log(party);
+    $scope.click = function (col, row) {
+        eoleWs.sessionPromise.then(function (ws) {
+            ws.publish('eole/games/tictactoe/parties/'+partyId, {
+                col: col,
+                row: row
+            });
+        });
+    };
 
-        $scope.party = party;
-        $scope.game = party.game;
+    eoleWs.sessionPromise.then(function (ws) {
+        ws.subscribe('eole/games/tictactoe/parties/'+partyId, function (topic, event) {
+            console.log('ttt event', topic, event);
+
+            switch (event.type) {
+                case 'init':
+                    $scope.party = event.party;
+                    $scope.game = event.party.game;
+                    $scope.tictactoe = event.tictactoe;
+                    break;
+
+                case 'move':
+                    $scope.tictactoe.grid = replaceAt(
+                        $scope.tictactoe.grid,
+                        event.move.col,
+                        event.move.row,
+                        event.move.symbol
+                    );
+                    $scope.tictactoe.last_move = event.move.symbol;
+                    break;
+            }
+
+            $scope.$apply();
+        });
     });
+
+    var replaceAt = function(string, col, row, character) {
+        var index = row * 3 + col;
+
+        return string.substr(0, index) + character + string.substr(index + character.length);
+    };
 }]);
