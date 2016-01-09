6314f9d2205e7a80d51f27d42c1ed3f6f8832a37
Julien Maulny <doubjulien@hotmail.fr>
2016-01-09 16:43:43 +0100

    awale css updates, dynamic view, load party data

--- ng-eole/games/awale/awale.js
+++ ng-eole/games/awale/awale.js
@@ -3,10 +3,35 @@
 ngEole.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/games/awale/parties/:partyId', {
         controller: 'AwaleController',
-        templateUrl: 'games/awale/awale.html'
+        templateUrl: 'ng-eole/games/awale/awale.html'
     });
 }]);
 
 ngEole.controller('AwaleController', ['$scope', '$routeParams', 'eoleApi', 'eoleWs', '$timeout', 'partyManager', 'eoleSession', function ($scope, $routeParams, eoleApi, eoleWs, $timeout, partyManager, eoleSession) {
+    var partyId = $routeParams.partyId;
+    var awaleParty = null;
+    $scope.grid = [
+        {
+            seeds: [0, 0, 0, 0, 0, 0],
+            attic: 0
+        },
+        {
+            seeds: [0, 0, 0, 0, 0, 0],
+            attic: 0
+        }
+    ];
+    $scope.party = {};
 
+    $scope.move = function (move) {
+        eoleApi.call('post', 'api/games/awale/play', eoleSession.player, {
+            move: move,
+            party_id: partyId
+        });
+    };
+
+    eoleApi.call('get', 'api/games/awale/find-by-id/'+partyId).then(function (data) {
+        awaleParty = data;
+        $scope.grid = awaleParty.grid;
+        $scope.party = awaleParty.party;
+    });
 }]);
