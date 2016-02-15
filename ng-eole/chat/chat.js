e0e906d1d32c99902300bc14847725c13a2d83b5
Julien Maulny <doubjulien@hotmail.fr>
2016-02-15 18:20:29 +0100

    chat style

--- ng-eole/chat/chat.js
+++ ng-eole/chat/chat.js
@@ -19,10 +19,14 @@ ngEole.controller('ChatController', ['$scope', '$translate', 'eoleWs', function 
 
     eoleWs.socketPromise.then(function (socket) {
         socket.subscribe('eole/core/chat', function (topic, event) {
+            var willScrollBottom = isBottom();
+
             switch (event.type) {
                 case 'join':
                     $translate('{player}.has.join.chat', { player: event.player.username }).then(function (message) {
                         $scope.messages.push({
+                            date: new Date(),
+                            player: event.player,
                             content: message
                         });
                     });
@@ -31,19 +35,52 @@ ngEole.controller('ChatController', ['$scope', '$translate', 'eoleWs', function 
                 case 'message':
                     $scope.$apply(function () {
                         $scope.messages.push({
-                            content: event.player.username+' > '+event.message
+                            date: new Date(),
+                            player: event.player,
+                            content: event.message
                         });
+
+                        if (willScrollBottom) {
+                            scrollBottom();
+                        }
                     });
                     break;
 
                 case 'leave':
                     $translate('{player}.has.left.chat', { player: event.player.username }).then(function (message) {
                         $scope.messages.push({
+                            date: new Date(),
+                            player: event.player,
                             content: message
                         });
                     });
                     break;
             }
+
+            if (willScrollBottom) {
+                scrollBottom();
+            }
         });
     });
+
+    var avatarUrlCache = {};
+
+    $scope.avatarUrl = function (player) {
+        if (!avatarUrlCache[player.id]) {
+            var color = CryptoJS.SHA1(player.id+''+player.date_created).toString().substr(0, 6);
+            var initials = player.username.substr(0, 2).toUpperCase();
+
+            avatarUrlCache[player.id] = 'http://placehold.it/50/'+color+'/fff&text='+initials;
+        }
+
+        return avatarUrlCache[player.id];
+    };
+
+    var isBottom = function () {
+        return (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 20;
+    };
+
+    var scrollBottom = function () {
+        window.scrollTo(0, document.body.scrollHeight);
+    };
 }]);
