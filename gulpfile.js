2e05c6f091c50a74000aaae65a95882044549f42
Julien Maulny <doubjulien@hotmail.fr>
2016-08-06 16:10:55 +0200

    refactor assets files list into assets.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var rename = require('gulp-rename');
 var bower = require('gulp-bower');
 var inject = require('gulp-inject');
 
+var eoleAssets = require('./assets.js');
+
 gulp.task('assets', [
     'inject-assets'
 ]);
@@ -37,7 +39,7 @@ gulp.task('inject-assets-prod', function () {
 });
 
 gulp.task('inject-assets', function () {
-    var devAssets = gulp.src([].concat(cssFiles, jsFiles));
+    var devAssets = gulp.src([].concat(eoleAssets.css, eoleAssets.js));
 
     return gulp
         .src('./index.html')
@@ -48,7 +50,7 @@ gulp.task('inject-assets', function () {
 
 gulp.task('build-css', function () {
     return gulp
-        .src(cssFiles)
+        .src(eoleAssets.css)
         .pipe(concat('eole.min.css'))
         .pipe(cleanCSS())
         .pipe(gulp.dest('./assets/css/'))
@@ -57,7 +59,7 @@ gulp.task('build-css', function () {
 
 gulp.task('build-js', function () {
     return gulp
-        .src(jsFiles)
+        .src(eoleAssets.js)
         .pipe(concat('eole.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./assets/js/'))
@@ -66,7 +68,7 @@ gulp.task('build-js', function () {
 
 gulp.task('build-fonts', function () {
     return gulp
-        .src(fontFiles)
+        .src(eoleAssets.fonts)
         .pipe(gulp.dest('./assets/fonts/'))
     ;
 });
@@ -113,103 +115,3 @@ gulp.task('copy-index-html', function () {
 
 gulp.task('default', function () {
 });
-
-var cssFiles = [
-    'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'eole/chat/css/chat.css',
-    'eole/common/css/all.css',
-
-    'eole/games/tictactoe/css/tictactoe.css',
-    'eole/games/tictactoe/css/layout.css',
-
-    'eole/games/awale/css/awale.css',
-
-    'eole/games/monkeys/css/card.css',
-    'eole/games/monkeys/css/monkeys.css'
-];
-
-var jsFiles = [
-    'bower_components/cryptojslib/rollups/sha1.js',
-    'bower_components/cryptojslib/rollups/sha512.js',
-    'bower_components/cryptojslib/components/enc-base64-min.js',
-    'bower_components/cryptojs-password-encoder/CryptoJsPasswordEncoder.js',
-    'bower_components/autobahn-old/autobahn.min.js',
-    'bower_components/angular/angular.js',
-    'bower_components/angular-locker/dist/angular-locker.min.js',
-    'bower_components/angular-translate/angular-translate.min.js',
-    'bower_components/angular-sanitize/angular-sanitize.min.js',
-    'bower_components/angular-route/angular-route.min.js',
-    'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-    'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-    'bower_components/tv4/tv4.js',
-    'bower_components/objectpath/lib/ObjectPath.js',
-    'bower_components/angular-schema-form/dist/schema-form.min.js',
-    'bower_components/angular-schema-form/dist/bootstrap-decorator.min.js',
-
-    'eole_components/relative-path/relative-path.js',
-    'eole_components/eole-api-client/eole-api-client.js',
-    'eole_components/eole-api-client/eole-api-client-mock.js',
-    'eole_components/eole-api-client/ng-module.js',
-    'eole_components/eole-websocket-client/eole-websocket-client.js',
-    'eole_components/eole-websocket-client/eole-websocket-client-mock.js',
-    'eole_components/eole-websocket-client/ng-module.js',
-
-    'config/environment.js',
-
-    'eole/module.js',
-    'eole/common/config/html5-mode.js',
-    'eole/common/config/locker.js',
-    'eole/common/service/party-manager.js',
-    'eole/common/service/eole-session.js',
-    'eole/common/service/websocket.js',
-    'eole/common/translations/module.js',
-    'eole/common/translations/config.js',
-    'eole/common/translations/schema-form.js',
-    'eole/common/translations/en.js',
-    'eole/common/translations/fr.js',
-    'eole/menu/module.js',
-    'eole/menu/controllers/menu.js',
-    'eole/home/module.js',
-    'eole/home/routes.js',
-    'eole/home/controllers/home.js',
-    'eole/player/module.js',
-    'eole/player/routes.js',
-    'eole/player/controllers/login.js',
-    'eole/player/controllers/profile.js',
-    'eole/player/controllers/register.js',
-    'eole/game/module.js',
-    'eole/game/routes.js',
-    'eole/game/controllers/game.js',
-    'eole/game/controllers/games.js',
-    'eole/default-game/module.js',
-    'eole/default-game/routes.js',
-    'eole/default-game/controllers/party.js',
-    'eole/chat/module.js',
-    'eole/chat/routes.js',
-    'eole/chat/controllers/chat.js',
-
-    'eole/games/tictactoe/module.js',
-    'eole/games/tictactoe/routes.js',
-    'eole/games/tictactoe/controllers/tictactoe.js',
-    'eole/games/tictactoe/translations/en.js',
-    'eole/games/tictactoe/translations/fr.js',
-
-    'eole/games/awale/module.js',
-    'eole/games/awale/routes.js',
-    'eole/games/awale/model/seeds.js',
-    'eole/games/awale/services/grid-manager.js',
-    'eole/games/awale/services/anim-highlight.js',
-    'eole/games/awale/controllers/awale.js',
-    'eole/games/awale/translations/en.js',
-    'eole/games/awale/translations/fr.js',
-
-    'eole/games/monkeys/module.js',
-    'eole/games/monkeys/routes.js',
-    'eole/games/monkeys/service/monkeys-api.js',
-    'eole/games/monkeys/service/croupier.js',
-    'eole/games/monkeys/controllers/party.js'
-];
-
-var fontFiles = [
-    'bower_components/bootstrap/dist/fonts/*.*'
-];
